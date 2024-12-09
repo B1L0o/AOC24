834efea560e1757046d22e17a5b4f6435ff24a26
B1L0o <robin.paulik33@gmail.com>
2024-12-09 16:12:44 +0000

    gold

---
+++ 7/main.cpp
@@ -0,0 +1,51 @@
+using namespace std;
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include "../utils/utils.h"
+
+
+bool is_valid(int n,vector<int>v,char c,size_t idx,int sum)
+{ 
+    if (v.size() == 0) return false;
+    if (v.size() == 1) return n == v[1];
+    if(idx == 0) 
+    {
+        sum = v[idx];
+        return is_valid(n,v,'+',1,sum) || is_valid(n,v,'*',1,sum);
+    }
+    if(idx == v.size()) 
+    {
+        return sum == n;
+    }
+    if (c == '+') sum += v[idx];
+    if (c == '*') sum *= v[idx];
+    return is_valid(n,v,'+',idx+1,sum) || is_valid(n,v,'*',idx+1,sum);
+}
+
+int main(void)
+{
+    string line;
+    long long num;
+    long long n;
+    long long res = 0;
+    while (getline(cin,line))
+    {
+        vector<int> v;
+        replace(line.begin(),line.end(), ':',' ');
+        stringstream stream(line);
+        stream >> num;
+        while(stream)
+        {
+            stream >> n;
+            v.push_back(n);
+        }
+        // last element should not be included
+        if(v.size() > 0)v.pop_back();
+        if (is_valid(num,v,0,0,0)) res += num;
+    }
+    cout << res;
+    return 0;
+}
